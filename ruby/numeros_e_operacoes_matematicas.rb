ea91fdcd05f45190a0ea1b975f5e9103bc931b43
Los-had <githubtestefinal@gmail.com>
2021-06-27 15:33:27 -0300

    Números e operações matemáticas

---
+++ ruby/numeros_e_operacoes_matematicas.rb
@@ -0,0 +1,16 @@
+a = 123123
+b = 456456
+soma = a + b
+puts soma
+c = 7
+d = 10
+subtracao = c - d
+puts subtracao
+e = 10
+f = 11
+multiplicacao = e * f
+puts multiplicacao
+g = 20
+h = 2
+divisao = g / h
+puts divisao
