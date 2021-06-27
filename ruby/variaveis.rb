5eaca1d862aa95d1e375c69b4be0f76e557622a4
Los-had <githubtestefinal@gmail.com>
2021-06-27 15:25:36 -0300

    tipos de variáveis

--- ruby/variaveis.rb
+++ ruby/variaveis.rb
@@ -2,5 +2,7 @@ puts "Qual é o seu nome?"
 nome = gets.chomp
 puts "Quantos anos você tem"
 idade = gets.chomp.to_i
+altura = 1.69
+interesses = ['Python', 'Sqlite', 'Ruby', 'php', 'MySql']
 
 puts "oi meu nome é #{nome} e eu tenho #{idade} anos"
