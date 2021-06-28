883e1b6e6cff617753c0187efa31dd1e9a3da02e
Los-had <githubtestefinal@gmail.com>
2021-06-28 20:03:18 -0300

    arrays

---
+++ ruby/arrays/array.rb
@@ -0,0 +1,7 @@
+lista = [30, 9, 'Miguel', ['Adoro ruby!', 'É muito legal']]
+lista.push("php,", " ruby,", " python", " = linguagens fodas demais")
+lista.insert(5, "Oi")
+lista.delete("Oi")
+puts lista#[2]
+puts lista.length
+puts lista.sort.first
