b077b2c7d8b416905d1f85a30aaeebfbb57133eb
Los-had <githubtestefinal@gmail.com>
2021-07-09 16:15:53 -0300

    funções

---
+++ ruby/metodos/calc.rb
@@ -0,0 +1,19 @@
+#como ruby
+
+def calc a, b 
+    a + b
+end 
+
+#como o python
+
+def calc_py(c, d)
+    return c + d
+end
+
+def sub(e, f)
+    return e - f
+end
+
+puts calc 1, 1
+puts calc_py(1, 1)
+puts sub(3, 2)
