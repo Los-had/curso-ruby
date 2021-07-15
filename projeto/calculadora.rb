4f52c08e7d03ec6e743f7c494654dfdfae5a11de
Miguel <79226657+Los-had@users.noreply.github.com>
2021-07-15 23:02:29 +0000

    nada

--- projeto/calculadora.rb
+++ projeto/calculadora.rb
@@ -1,16 +1,16 @@
 puts "Bem vindo a calculadora!"
 puts "--------------"
 def somar(a, b)
-    puts a + b
-    menu()
+  puts a + b
+  menu()
 end
 def subtrair(c, d)
-    puts c - d
-    menu()
+  puts c - d
+  menu()
 end 
 def multiplicar(e, f)
-    puts e * f
-    menu()
+  puts e * f
+  menu()
 end 
 def dividir(g, h)
   if g == 0
@@ -25,35 +25,35 @@ def dividir(g, h)
   end
 end
 def menu()
-    puts "1.Somar\n2.Subtrair\n3.Dividir\n4.Multiplicar\n--------------"
-    choice_of_calc = gets.chomp
-    case choice_of_calc
-    when '1'
-        puts "Primeiro número"
-        num1 = gets.chomp.to_i
-        puts "Segundo número"
-        num2 = gets.chomp.to_i
-        somar(num1, num2)
-    when '2'
-        puts "Primeiro número"
-        num1 = gets.chomp.to_i
-        puts "Segundo número"
-        num2 = gets.chomp.to_i
-        subtrair(num1, num2)
-    when '3'
-        puts "Primeiro número"
-        num1 = gets.chomp.to_i
-        puts "Segundo número"
-        num2 = gets.chomp.to_i
-        dividir(num1, num2)
-    when '4'
-        puts "Primeiro número"
-        num1 = gets.chomp.to_i
-        puts "Segundo número"
-        num2 = gets.chomp.to_i
-        multiplicar(num1, num2)
-    else
-      puts "Operação inválida."
-    end 
+  puts "1.Somar\n2.Subtrair\n3.Dividir\n4.Multiplicar\n--------------"
+  choice_of_calc = gets.chomp
+  case choice_of_calc
+  when '1'
+    puts "Primeiro número"
+    num1 = gets.chomp.to_i
+    puts "Segundo número"
+    num2 = gets.chomp.to_i
+    somar(num1, num2)
+  when '2'
+    puts "Primeiro número"
+    num1 = gets.chomp.to_i
+    puts "Segundo número"
+    num2 = gets.chomp.to_i
+    subtrair(num1, num2)
+  when '3'
+    puts "Primeiro número"
+    num1 = gets.chomp.to_i
+    puts "Segundo número"
+    num2 = gets.chomp.to_i
+    dividir(num1, num2)
+  when '4'
+    puts "Primeiro número"
+    num1 = gets.chomp.to_i
+    puts "Segundo número"
+    num2 = gets.chomp.to_i
+    multiplicar(num1, num2)
+  else
+  puts "Operação inválida."
+  end 
 end 
 menu()
