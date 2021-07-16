863551d00161a86b85d4f78fed9681985d04c240
Miguel <79226657+Los-had@users.noreply.github.com>
2021-07-16 10:50:56 +0000

    gerador de senhas

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ calculadora que faz soma, subtração, divisão e multiplicação
 Gerador de números aleatórios entre dois valores(minimo e maximo)
 ## reverse
 inverte a string digitada pelo usuário utilizando o método reverse
+## Gerador de senhas 
+Gerador de senhas que gera senhas de tamanho personalizado e salva em um arquivo. E também criptografa a senha se o usuário desejar utilizando o módulo SecureRandom
---
+++ projeto/gerador_de_senhas.rb
@@ -0,0 +1,41 @@
+require 'securerandom'
+
+def menu()
+  puts "Seja bem vindo ao gerador de senhas"
+  puts "---------------------------------------"
+  def password_generator()
+    puts "Quantos caracteres deseja na senha?"
+    ndc = gets.chomp.to_i
+    senha = ""
+    ndc.times do
+      senha += (rand(128).chr)
+    end
+    puts senha
+    puts "Deseja salvar senha em um arquivo?(y/n)"
+    choice_of_save_password = gets.chomp
+    if choice_of_save_password == "y"
+      puts "Deseja criptografar senha antes de salvar no arquivo?(y/n)"
+      encryption_choice = gets.chomp
+      if encryption_choice == "y"
+        nova_senha = SecureRandom.base64()
+        arq = File.new('senha.txt', 'a')
+        arq.write(nova_senha)
+        menu()
+      elsif encryption_choice == "n"
+        arq = File.new('senha.txt', 'a')
+        arq.write(senha)
+        menu()
+      else
+        puts "'#{encryption_choice}': é inválido'"
+        menu()
+      end
+    elsif choice_of_save_password == "n"
+      menu()
+    else
+      puts "'#{choice_of_save_password}': é inválido"
+      menu()
+    end
+  end
+  password_generator()
+end
+menu()
